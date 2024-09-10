ae1c918b8d7ee3f1e9845cf3ea792c216f7ed8e9
Javier <javieralzuu@gmail.com>
2024-09-10 16:47:53 -0300

    nav mobile ready

---
+++ src/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleMenu = document.getElementById('toggle-menu');
+    const navList = document.getElementById('nav-list');
+    const closeMenu = document.getElementById('close-menu');
+  
+    // Abrir el menú cuando se hace clic en el ícono del menú
+    toggleMenu.addEventListener('click', function () {
+      navList.classList.add('show-menu'); // Agrega la clase 'show-menu'
+      toggleMenu.style.display = 'none'; // Oculta el ícono del menú
+      closeMenu.style.display = 'block'; // Muestra el ícono de cerrar
+    });
+  
+    // Cerrar el menú cuando se hace clic en el ícono de cerrar
+    closeMenu.addEventListener('click', function () {
+      navList.classList.remove('show-menu'); // Remueve la clase 'show-menu'
+      closeMenu.style.display = 'none'; // Oculta el ícono de cerrar
+      toggleMenu.style.display = 'block'; // Muestra el ícono del menú
+    });
+  });
+  
